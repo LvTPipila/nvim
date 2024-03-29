518d3670839addc552c87b9e69922b191b885ca4
Carlos Rodriguez <rgx220dz@protonmail.com>
2024-03-29 10:55:27 +0100

    Add theme vim options

--- lua/options.lua
+++ lua/options.lua
@@ -9,19 +9,24 @@ vim.opt.mouse = "n"
 vim.opt.guicursor = ""
 
 -- Tab
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 
 -- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+
 
 vim.opt.swapfile = true
 vim.opt.backup = false
 
+-- Theme
+vim.opt.syntax = "ON"
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "100"
+
